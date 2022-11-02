97bbfea8c49081e614c38633286216183e348e74
fooname <117125968+Foooname@users.noreply.github.com>
2022-11-02 15:57:07 -0400

    Tarea 2 - LargeCatus

--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -3,6 +3,6 @@ from .obstacle import Obstacle
 
 class Cactus(Obstacle):
     def __init__(self, images):
-        self.type = random.randint(0, 2)
+        self.type = random.randint(0, 5)
         super().__init__(images, self.type)
-        self.rect.y = 325
+        self.rect.y = random.randint(319, 322)
