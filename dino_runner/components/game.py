7d910c728cbec145c7a70940b667b390691a8736
fooname <117125968+Foooname@users.noreply.github.com>
2022-11-02 15:29:58 -0400

    Clase 2

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,6 +1,10 @@
 import pygame
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+
+from dino_runner.components.obstacles.cactus import Cactus
+from dino_runner.components.obstacles.obstacles_manager import ObstacleManager
+from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, SMALL_CACTUS, TITLE, FPS
 from dino_runner.components.dinosaur import Dinosaur
 
 
@@ -16,7 +20,8 @@ class Game:
         self.x_pos_bg = 0
         self.y_pos_bg = 380
         self.player = Dinosaur()
-    
+        self.obstacle_manager = ObstacleManager()
+
     def run(self):
         # Game loop: events - update - draw
         self.playing = True
@@ -30,17 +35,21 @@ class Game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.playing = False
-
+    
     def update(self):
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
-
+        self.obstacle_manager.update(self)
+    
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.player.draw(self.screen)
+        self.obstacle_manager.draw(self)
         pygame.display.update()
         pygame.display.flip()
+
 
     def draw_background(self):
         image_width = BG.get_width()
